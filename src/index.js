f7178365fc85c7fb870cdbe33edad9c024b47ded
Sam Atkins <sam@cubi.io>
2017-03-15 15:26:17 +0000

    chore: Enable Styl and CSS

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
 
-// import './css/style.css';
+import './styles/style.css';
 
 render(<App />, document.querySelector('#main'));
